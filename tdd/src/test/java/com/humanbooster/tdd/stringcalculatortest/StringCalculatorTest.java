dd9f6871f1379a29cc12eecc7aff10db7f0ee7b5
JeromePrevotat <jer.prevotat@gmail.com>
2025-06-03 10:50:05 +0200

    String Calculator all test

--- tdd/src/test/java/com/humanbooster/tdd/stringcalculatortest/StringCalculatorTest.java
+++ tdd/src/test/java/com/humanbooster/tdd/stringcalculatortest/StringCalculatorTest.java
@@ -6,10 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import com.humanbooster.tdd.stringcalculator.StringCalculator;
 
 public class StringCalculatorTest {
+
+    /////////////////////////////////////////////////////////////////////////////////
+    /// INVALIDATION TESTS                                                        ///
+    /////////////////////////////////////////////////////////////////////////////////
+
     @Test
     public void input_null_should_return_null_exeption() {
         assertThrowsExactly(NullPointerException.class, () -> {
@@ -78,5 +85,121 @@ public class StringCalculatorTest {
         });
     }
 
-    
+    /////////////////////////////////////////////////////////////////////////////////
+    /// VALIDATION TESTS                                                          ///
+    /////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void input_with_two_single_digits_should_return_the_sum() throws Exception {
+        assertEquals(6, new StringCalculator().add("4,2"));
+    }
+
+    @Test
+    public void input_with_a_single_digit_and_a_two_digits_number_should_return_the_sum() throws Exception {
+        assertEquals(42, new StringCalculator().add("1, 41"));
+    }
+
+    @Test
+    public void input_with_two_two_digits_numbers_should_return_the_sum() throws Exception {
+        assertEquals(100, new StringCalculator().add("50,50"));
+    }
+
+    @Test
+    @ParameterizedTest
+    @CsvSource(
+        {
+            //"expected, number1, number2",
+            "42,21,21",
+            "9,6,3",
+            "10,9,1",
+            "101,99,2",
+            "1000,500,500",
+            "999,666,333",
+            "1000,999,1",
+            "1001,999,2",
+            "1010,1000,10",
+            "1515,1000,515",
+            "1998,999,999",
+        }
+    )
+    public void input_with_random_number_of_digits_should_return_the_sum_of_the_two(int expected, int number1, int number2) throws Exception {
+        assertEquals(expected, new StringCalculator().add(number1 + "," + number2));
+    }
+
+    @Test
+    @ParameterizedTest
+    @CsvSource(
+        {
+            //"expected, number1, number2",
+            "190,90,60,40",
+            "175,75,60,40",
+        }
+    )
+    public void input_with_random_number_of_digits_should_return_the_sum_of_all_3(int expected, int n1, int n2, int n3) throws Exception {
+        assertEquals(expected, new StringCalculator().add(n1 + "," + n2 + "," + n3));
+    }
+
+    @Test
+    @ParameterizedTest
+    @CsvSource(
+        {
+            //"expected, number1, number2",
+            "315,150,10,80,75",
+            "540,150,50,90,250",
+        }
+    )
+    public void input_with_random_number_of_digits_should_return_the_sum_of_all_4(int expected, int n1, int n2, int n3, int n4) throws Exception {
+        assertEquals(expected, new StringCalculator().add(n1 + "," + n2 + "," + n3 + "," + n4));
+    }
+
+    @Test
+    @ParameterizedTest
+    @CsvSource(
+        {
+            //"expected, number1, number2",
+            "540,250,40,200,10,40",
+            "370,200,10,60,10,90",
+        }
+    )
+    public void input_with_random_number_of_digits_should_return_the_sum_of_all_5(int expected, int n1, int n2, int n3, int n4, int n5) throws Exception {
+        assertEquals(expected, new StringCalculator().add(n1 + "," + n2 + "," + n3 + "," + n4 + "," + n5));
+    }
+
+    @Test
+    @ParameterizedTest
+    @CsvSource(
+        {
+            //"expected, number1, number2...",
+            "390,100,100,60,40,90",
+            "750,75,300,25,50,50,250"
+        }
+    )
+    public void input_with_random_number_of_digits_should_return_the_sum_of_all_6(int expected, int n1, int n2, int n3, int n4, int n5, int n6) throws Exception {
+        assertEquals(expected, new StringCalculator().add(n1 + "," + n2 + "," + n3 + "," + n4 + "," + n5 + "," + n6));
+    }
+
+    @Test
+    @ParameterizedTest
+    @CsvSource(
+        {
+            //"expected, number1, number2...",
+            "390,100,100,60,40,90",
+            "750,75,300,25,50,50,250"
+        }
+    )
+    public void input_with_newline_separator_with_random_number_of_digits_should_return_the_sum_of_all_6(int expected, int n1, int n2, int n3, int n4, int n5, int n6) throws Exception {
+        assertEquals(expected, new StringCalculator().add(n1 + "\n" + n2 + "\n" + n3 + "\n" + n4 + "\n" + n5 + "\n" + n6));
+    }
+
+    @Test
+    @ParameterizedTest
+    @CsvSource(
+        {
+            //"expected, number1, number2...",
+            "//!\n390,100,100,60,40,90",
+            "//!\n750,75,300,25,50,50,250"
+        }
+    )
+    public void input_with_custom_separator_with_random_number_of_digits_should_return_the_sum_of_all_6(int expected, int n1, int n2, int n3, int n4, int n5, int n6) throws Exception {
+        assertEquals(expected, new StringCalculator().add(n1 + "!" + n2 + "!" + n3 + "!" + n4 + "!" + n5 + "!" + n6));
+    }
 }
